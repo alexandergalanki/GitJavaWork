e121c7b0e764b41b204d35dff40226f4f392a090
unknown <galankialex@gmail.com>
2019-11-05 17:31:13 +0530

    okkk

---
+++ javafundamentals/src/Starr.java
@@ -0,0 +1,22 @@
+package javafundamentals;
+
+public class Starr
+{
+
+public static void main(String[] args) 
+{
+ int i, j, k;
+for(i=3;i>=1;i--)
+{
+for(j=3;j>i;j--)
+{
+System.out.print(" ");
+}
+for(k=1;k<(i*2);k++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
