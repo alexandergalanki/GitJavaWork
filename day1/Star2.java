6209595de992f6eb789acabae02377a815427645
unknown <galankialex@gmail.com>
2019-11-06 09:40:28 +0530

    pattern

---
+++ day1/Star2.java
@@ -0,0 +1,20 @@
+public class Star2
+{
+
+public static void main(String[] args) 
+{
+ int i, j, k;
+for(i=5;i>=4;i--)
+{
+for(j=5;j>i;j--)
+{
+System.out.print(" ");
+}
+for(k=1;k<(i*2);k++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
