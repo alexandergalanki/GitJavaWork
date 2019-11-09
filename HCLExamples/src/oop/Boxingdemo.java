dcd43bbce4be676f09b5e2b3b7e48fed6ae8ca7f
unknown <galankialex@gmail.com>
2019-11-09 12:34:35 +0530

    NOV9

---
+++ HCLExamples/src/oop/Boxingdemo.java
@@ -0,0 +1,30 @@
+//boxing-unboxing example
+package oop;
+
+public class Boxingdemo 
+{
+	public static void main (String args[])
+	{
+	int a=0;
+	int b=0;
+	
+	if(args.length!=0)
+	{
+		//unboxing 
+		//convert String object into int value
+		
+		a=Integer.parseInt(args[0]);
+		
+		b=Integer.parseInt(args[1]);
+		
+		Integer result=a+b;
+		
+		System.out.println(result);
+	}
+	else
+	{
+		System.out.println("Please pass two integers as arguments!");
+	}
+	}
+
+}
